9dc8eed7375fd5d14c5d336aeeefef74f430d0fd
Bas Paap <paap@kissthefrog.nl>
2022-08-12 12:10:18 +0200

    Add DOTween

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(AudioSource))]
 public class Clock : MonoBehaviour
@@ -10,6 +11,7 @@ public class Clock : MonoBehaviour
     [SerializeField] private float numSecondsAtStart;
     [SerializeField] private TextMeshProUGUI timeText;
     [SerializeField] private TextMeshProUGUI fractionText;
+    [SerializeField] private TextMeshProUGUI doubleOhSevenText;
     [SerializeField] private TextMeshProUGUI gameOverText;
     [SerializeField] private AudioSource audioPlayer;
     [SerializeField] private float audioStartTime = 7 + 1.2f;
@@ -20,14 +22,16 @@ public class Clock : MonoBehaviour
     private float startTime;
     private int lastBeepSecond;
     private AudioSource audioSource;
+    private bool isTimeVisible = true;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         startTime = Time.time;
         gameOverText.color = Color.clear;
+        doubleOhSevenText.color = Color.clear;
     }
-    
+
     private void Update()
     {
         var numElapsedSeconds = Time.time - startTime;
@@ -36,13 +40,13 @@ public class Clock : MonoBehaviour
 
         string timeFormat = "h':'mm':'ss";
         string fractionFormat = "'.'ff";
-    
+
         if (numSecondsRemaining > 7 && (int)numSecondsRemaining != lastBeepSecond)
         {
             lastBeepSecond = (int)numSecondsRemaining;
             if (numSecondsRemaining < pitchIncreaseTime)
             {
-                audioSource.pitch = 1 +  (pitchIncreaseTime - numSecondsRemaining) / pitchIncreaseTime;
+                audioSource.pitch = 1 + (pitchIncreaseTime - numSecondsRemaining) / pitchIncreaseTime;
             }
             audioSource.Play();
         }
@@ -62,13 +66,25 @@ public class Clock : MonoBehaviour
         }
         if (numSecondsRemaining < doubleOhSevenDisplayTime)
         {
-            timeText.text = "0:07";
-            fractionText.text = string.Empty;
+            const float timeFadeDuration = 1.0f;
+
+            timeText.text = "0:00:07";
+            fractionText.text = ".00";
+
+            if (isTimeVisible)
+            {
+                doubleOhSevenText.color = Color.red;
+                timeText.DOKill();
+                timeText.DOFade(0, timeFadeDuration);
+                fractionText.DOKill();
+                fractionText.DOFade(0, timeFadeDuration);
+                isTimeVisible = false;
+            }
         }
         else
         {
             timeText.text = timeToDisplay.ToString(timeFormat);
             fractionText.text = timeToDisplay.ToString(fractionFormat);
-        }        
+        }
     }
 }
